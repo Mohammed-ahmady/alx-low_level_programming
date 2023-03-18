9ef43b8a475a9f35c90961af0a3ccfa52e2c807f
Mohamed-ahmady <mohamedahmady404@gmail.com>
2023-03-17 18:28:15 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the value of n status:
+ *              greater than, is zero and is not less than 6.
+ *
+ * Return: Always O (Success)
+*/
+
+int main(void)
+{
+	char c = 'z';
+
+	for (c = 'z'; c >= 'a'; --c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+
+
+}
