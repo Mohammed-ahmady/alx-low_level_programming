36aaea9e982a519dd91a1a466332bde8ea4990cb
Mohamed-ahmady <mohamedahmady404@gmail.com>
2023-03-17 18:18:56 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+		printf("%i", n);
+	printf("\n");
+	return (0);
+}
