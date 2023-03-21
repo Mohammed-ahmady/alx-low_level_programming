f6228d68348d94534f726ce900af41cd17489ba4
Mohamed-ahmady <mohamedahmady404@gmail.com>
2023-03-21 15:19:20 -0700

    betty style error

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * add - adds two integers and returns the result.
- *
- * add : is function.
+ *add -  adds two integers and returns the result.
  *
  * result : is the value of adding the 2 integers.
  *
- *@a : input integer
+ *a : input integer
  *
- *@b :  second input integer
+ *b :  second input integer
  *
- *Return : The value of result
+ * Return : The value of result
  */
 
 int add(int a, int b)
 {
 	int result = a + b;
 
+	_putchar(result + '0');
 	return (result);
 }
