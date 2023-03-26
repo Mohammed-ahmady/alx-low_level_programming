8d1214a28d9ea31c7154180ecda474a7d01e29ff
Mohamed-ahmady <mohamedahmady404@gmail.com>
2023-03-26 16:34:32 -0700

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,19 +13,24 @@ void print_triangle(int size)
 	int i;
 	int j;
 
-	for (i = 0; i <= size; i++)
-	{
-		for (j = 0; j <= size; j++)
-		{
-			if (j + i <= size)
-			{
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('#');
-			}
-		}
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j + i <= size)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
 	}
 }
