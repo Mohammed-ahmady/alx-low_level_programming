d86b2b271c1632bc71229da2fb5c89b2a1acc743
Mohamed-ahmady <mohamedahmady404@gmail.com>
2023-04-10 04:37:53 -0700

    Checker nm libmy.a !

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,36 @@
+/**
+ * _strspn - a function that gets the
+ *           length of a prexif substring
+ *
+ * @s: pointer to string input
+ * @accept: substring prefix to look for
+ *
+ * Return: the number of bytes in the initial segment
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, v;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		v = 1;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				v = 0;
+				break;
+			}
+			j++;
+		}
+		if (v == 1)
+			break;
+		i++;
+	}
+
+	return (i);
+}
+
